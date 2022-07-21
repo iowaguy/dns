8fbdbff43c9d84fa1f54233e98f4942a91e0b888
Ben Weintraub <bweintraub@63zk7m3>
2022-07-21 15:47:05 -0500

    Add fake data to DNSSEC serialization test

--- dnssec_serialization_test.go
+++ dnssec_serialization_test.go
@@ -5,17 +5,43 @@ import (
 )
 
 func TestCopyDNSSEC(t *testing.T) {
-	rrs := []RR{
-		&Signature{
-			length: 1,
-			algorithm: 1,
-			ttl: 1,
-			expires: 1,
-			begins: 1,
-			key_tag: 1,
-			signature: "123",
-		},
+	sig := &Signature{
+		length: 1,
+		algorithm: 1,
+		ttl: 1,
+		expires: 1,
+		begins: 1,
+		key_tag: 1,
+		signature: "123",
 	}
+	dnskey_rdata := &DNSKEY_Rdata{
+		flags: 1,
+		protocol: 1,
+		algorithm: 1,
+		public_key: "abc",
+	}
+	key := &Key{length: 1, []DNSKEY_Rdata{*dnskey_rdata}}
+	entry := &Entering{
+		length: 1,
+		zType: EnteringType,
+		entry_key_index: 1,
+		key_sig: *sig,
+		num_keys: 1,
+		keys: []Key{*key},
+	}
+	exit := &Leaving{
+		length: 1,
+		zType: LeavingType,
+		next_name: "123",
+		rrtype: RRType(TypeTXT),
+		rrsig: *sig,
+	}
+	zp := &ZonePair{
+		entry: *entry,
+		exit: *exit,
+	}
+
+	rrs := []RR{*zp}
 
 	for _, rr := range rrs {
 		rr1 := rr.copy()
