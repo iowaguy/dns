d0c563c3beda462b3f46ad9f76e632dadef2450d
Ben Weintraub <bweintraub@63zk7m3>
2022-07-22 16:49:12 -0500

    Implement parse and duplicate stubs for DNSSEC
    Methods needed for DNSSEC serialization structs to implment RR interface.

--- dnssec_duplicate.go
+++ dnssec_duplicate.go
@@ -1,5 +1,53 @@
 package dns
 
-func (r1 *Signature) isDuplicate(_r2 RR) bool {
+func (rr *ZonePair) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *DNSSECProof) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *Signature) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *Key) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *DNSKEY_Rdata) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *Entering) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *SerialDS) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *Leaving) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *LeavingCNAME) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *LeavingDNAME) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *LeavingDS) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *LeavingOther) isDuplicate(_r2 RR) bool {
+	return false
+}
+
+func (rr *RRData) isDuplicate(_r2 RR) bool {
 	return false
 }
--- dnssec_parse.go
+++ dnssec_parse.go
@@ -1,6 +1,58 @@
 package dns
 
+// NOTE This is for parsing from zone record format. We will
+// not support this right now, but the methods need to be
+// implemented for the DNSSEC serialization types to implement
+// the RR interface.
+
+func (rr *ZonePair) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *DNSSECProof) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
 
 func (rr *Signature) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *Key) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *DNSKEY_Rdata) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *Entering) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *SerialDS) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *Leaving) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *LeavingCNAME) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *LeavingDNAME) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *LeavingDS) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *LeavingOther) parse(c *zlexer, o string) *ParseError {
+	return &ParseError{}
+}
+
+func (rr *RRData) parse(c *zlexer, o string) *ParseError {
 	return &ParseError{}
 }
